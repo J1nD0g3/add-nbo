ee976b7a5b78f0ac00e41b7d1430ff479bb383c6
J1nD0g3 <heojin2003@korea.ac.kr>
2024-03-14 07:57:22 -0400

    assignment2

---
+++ Makefile
@@ -0,0 +1,12 @@
+CC=gcc
+TARGET=add-nbo
+OBJS=add-nbo.o
+
+$(TARGET) : $(OBJS)
+	$(CC) -o $@ $(OBJS)
+
+add-nbo.o : add-nbo.c
+	$(CC) -c -o add-nbo.o add-nbo.c
+
+clean:
+	rm $(OBJECT) $(TARGET)
---
+++ add-nbo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <stdint.h>
+
+uint32_t read_num(char* f_name)
+{
+	FILE* f = fopen(f_name, "rb");
+	
+	uint32_t value;
+	fread(&value, sizeof(uint32_t), 1, f);
+	uint32_t result = ntohl(value);
+	
+	fclose(f);
+	return result;
+}
+
+int main(int argc, char *argv[]){
+	char* f1_name = argv[1];
+	char* f2_name = argv[2];
+
+	uint32_t f1_value = read_num(f1_name);
+	uint32_t f2_value = read_num(f2_name);
+	uint32_t sum = f1_value + f2_value;
+
+	printf("%d\(0X%X\) + %d\(0X%X\) = %d\(0X%X\)",f1_value, f1_value, f2_value, f2_value, sum, sum);
+	return 0;
+}
