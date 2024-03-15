5e2a602fec311232dd4711402905938d7a6b5b00
J1nD0g3 <heojin2003@korea.ac.kr>
2024-03-15 04:12:25 -0400

    add Error : file size < 4 bytes

--- add-nbo.c
+++ add-nbo.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 uint32_t read_num(char* f_name)
 {
 	FILE* f = fopen(f_name, "rb");
+
+	fseek(f, 0, SEEK_END);
+	int filesize = ftell(f);
+	if(filesize < 4){
+		printf("Error : file size is less than 4 bytes.\n");
+		exit(0);
+	}
 	
+	fseek(f, 0, SEEK_SET);
 	uint32_t value;
 	fread(&value, sizeof(uint32_t), 1, f);
 	uint32_t result = ntohl(value);
